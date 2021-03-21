990817cf325a092bb13459b8dde6aac222c4b638
Auliyaa <urushiraa@gmail.com>
2021-03-21 17:16:15 +0100

    [ender-3-2.0.7.2] adapt bed size & y endstop position for linear rail guide

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1131,11 +1131,11 @@
 
 // The size of the print bed
 #define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -2
-#define Y_MIN_POS -15
+#define Y_MIN_POS -36
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
